0940195c9fe7e8e5037030241b29d994e840c624
kasahiti <kastriot.sahiti@adron.ch>
2023-02-24 15:25:14 +0100

    Diagrams

--- backend/setup.sh
+++ backend/setup.sh
@@ -2,6 +2,7 @@
 sudo apt install openjdk-17-jdk python3.9 python3-pip git
 sudo ln -s /usr/bin/python3.9 /usr/bin/python
 sudo pip install check50
+sudo pip install check50-java
 sudo mkdir /kryptonite-students
 sudo chown -R debian: /kryptonite-students/
 sudo chmod 775 /kryptonite-students/
